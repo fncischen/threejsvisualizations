6efe46eee24e8e705ec6b91dab3c1293035e0fc7
fncischen <fncis.chen@gmail.com>
2020-11-16 22:03:15 -0800

    set up rough hierarchy to handle story templates for clients

--- src/components/storyTypes/verticalStory/verticalStory.js
+++ src/components/storyTypes/verticalStory/verticalStory.js
@@ -2,6 +2,8 @@ export default class verticalStory {
     constructor(props){
         this.characters = props.characters;
         this.environments = props.environments;
+        this.groundPlane = props.groundPlane; 
+
         this.storyIndex = props.storyIndex;  
         this.storyUI = props.storyUI;
     }
@@ -11,7 +13,7 @@ export default class verticalStory {
     }
 
     startStoryUI(){
-        
+
     }
 
 }
--- src/components/storyTypes/verticalStory/verticalStoryDashboard.js
+++ src/components/storyTypes/verticalStory/verticalStoryDashboard.js
@@ -1,8 +1,14 @@
+import * as THREE from 'three';
+// insert a line to import data from JSON file
+
 export default class VerticalStoryDashboard {
 
     constructor(props) {
         this.stories = props.stories;
         this.camera = props.camera; 
+        
+
+        this.initalizeStory();
     }
 
     onScreenScrollDown(dy) {
@@ -18,12 +24,25 @@ export default class VerticalStoryDashboard {
     }   
     
     onCameraPanRight(dx) {
-        
+
     }
 
     // convert 3d objects from storySpace to worldSpace
     convertStorySpaceToWorldSpace(storyIndex) {
+        let story = this.stories[index];
 
+        // assume ground plane is imported from box 
+        let groundPlane = story.groundPlane; 
+        let groundPlaneWidth = groundPlane.width; 
+        let groundPlaneCoords = groundPlane.transformCoords; 
+
+
+    }
+
+    initalizeStory() {
+        for(var i = 0; i < this.stories.length; i++){
+            this.convertStorySpaceToWorldSpace(i);
+        }
     }
 
 }
