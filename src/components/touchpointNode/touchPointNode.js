b4e33ab38fe8ef1d6f8aa8d81d83a2e2c1ba6d17
fncischen <fncis.chen@gmail.com>
2020-11-16 17:34:34 -0800

    created base touchpoint code for client AR installation art templates

---
+++ src/components/touchpointNode/touchPointNode.js
@@ -0,0 +1,38 @@
+export default class touchPointNode {
+
+    constructor(props){
+        this.data = props.data;
+        this.touchPointElements = props.touchPointElements;
+        this.touchPointStream = props.touchPointStream;
+    }
+
+    onTouchPointStart() {
+
+    }
+
+    onTouchPointPause() {
+
+    }
+
+    onTouchPointUpdate() {
+
+    }
+
+    // in case the audience decides to move the touchpoint
+    onTouchPointMove(){
+
+    }
+
+    onTouchPointEnd() {
+
+    }
+
+    onTouchPointSelect() {
+
+    }
+
+    onTouchPointHover() {
+
+    }
+
+}
