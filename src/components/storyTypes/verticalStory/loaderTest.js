370b7fe66a00e22d0e93f6ea13e572122bf5c5b4
fncischen <fncis.chen@gmail.com>
2020-11-18 14:01:51 -0800

    set up camera helper

--- src/components/storyTypes/verticalStory/loaderTest.js
+++ src/components/storyTypes/verticalStory/loaderTest.js
@@ -1,61 +1,78 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {useLoader} from "react-three-fiber";
+import {useLoader, useResource} from "react-three-fiber";
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
 
 export default function LoaderTest(){
 
         // https://threejsfundamentals.org/threejs/lessons/threejs-load-gltf.html
-        let objectsInScene;
-        let fun;
+        // let objectsInScene = [];
+        // let fun;
 
+        // // const loader = new GLTFLoader();
+        // console.log("test me out");
         // const loader = new GLTFLoader();
-        console.log("test me out");
-        const loader = new GLTFLoader();
-        console.log(loader);
+        // console.log(loader);
 
-        let scene = new THREE.Scene();
-        console.log(scene + " scene");
+        // let scene = new THREE.Scene();
+        // console.log(scene + " scene");
 
-        loader.load("/models/models2.gltf", function(gltf) {
-            console.log('test');
-            console.log(gltf.scene.children);
-            objectsInScene = gltf.scene;
-            fun = gltf.scene.children;
-            console.log( fun)
+        // loader.load("/models/models2.gltf", function(gltf) {
+        //     console.log('test');
+        //     console.log(gltf.scene.children);
+        //     objectsInScene = gltf.scene;
+        //     fun = gltf.scene.children;
+        //     console.log( fun)
 
-            scene.add(objectsInScene);
+        //     scene.add(objectsInScene);
 
-        },
-        function ( xhr ) {
+        // },
+        // function ( xhr ) {
 
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        //     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
     
-        },
-        // called when loading has errors
-        function ( error ) {
-            console.log("error : "  + error )
-            console.log( 'An error happened' );
+        // },
+        // // called when loading has errors
+        // function ( error ) {
+        //     console.log("error : "  + error )
+        //     console.log( 'An error happened' );
     
-        }
-        );
+        // }
+        // );
 
-         console.log(objectsInScene)
-         console.log(scene);
+        //  console.log(objectsInScene)
+        //  console.log(scene.children);
          // console.log(fun);
         
-        //  const { usedLoader } = useLoader(GLTFLoader, "/models/models2.gltf");
-         const {nodes, animations} = useLoader(GLTFLoader, "/models/models2.gltf");
+         var gltf = useLoader(GLTFLoader, "/models/models2.gltf", loader =>{
+            const dracoLoader = new DRACOLoader()
+            dracoLoader.setDecoderPath('/draco-gltf/')
+            loader.setDRACOLoader(dracoLoader)
+         });
 
-         // https://stackoverflow.com/questions/50674478/javascript-array-looks-like-its-empty-but-has-a-value
-         setTimeout(function(){
-            console.log(objectsInScene)
-         }, 1000)
+         console.log(gltf.scene.children);
+         console.log(gltf.scene.children[11]);
+        // const {nodes, animations} = useLoader(GLTFLoader, "/models/models2.gltf");
+        // var newnew = useResource();
+        //  // https://stackoverflow.com/questions/50674478/javascript-array-looks-like-its-empty-but-has-a-value
+        //  setTimeout(function(){
+        //     console.log(scene.children)
+        //     newnew = scene.children;
+        //  }, 1000)
 
-         console.log(nodes);
+         // question: how to transfer from settime out to return  
+
+
+         // console.log(nodes['Cube003']);
+
+
+         // console.log(nodes);
         //  console.log(usedLoader);
 
         return(
-            <mesh/>
+            <primitive object={gltf.scene}/>
+            
         )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom'
-import React, { useCallback, useEffect, useRef, useMemo } from 'react'
+import React, {Suspense, useCallback, useEffect, useRef, useMemo } from 'react'
 import Particles from "./components/particles/particles.js"
 import { Canvas, useFrame, useThree, extend} from 'react-three-fiber'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import LoaderTest from "./components/storyTypes/verticalStory/loaderTest.js"
+import {GUI} from '/jsm/libs/dat.gui.module'
+
 
 extend({ OrbitControls });
 
@@ -22,33 +24,44 @@ const CameraControls = (() => {
   const controls = useRef();
   // const Camera = new THREE.PerspectiveCamera({fov: 60, position: [0, 600, 600], near: 0.1, far: 5000});
   camera.position.x = 0;
-  camera.position.y = 600;
-  camera.position.z = 600;
+  camera.position.y = 1;
+  camera.position.z = 1;
   camera.fov = 60;
   camera.near = 0.1;
   camera.far = 5000;
 
+  // const cameraHelper = new THREE.CameraHelper(camera);
+  // generate bounding box for the camera 
+  // I want to be able to debug 
+
   /// https://codeworkshop.dev/blog/2020-04-03-adding-orbit-controls-to-react-three-fiber/
   useFrame(() => {controls.current.update()})
 
-  return <orbitControls ref={controls} args={[camera, domElement]} autoRotate={false} enableZoom={true} />
+  return( 
+  <group>
+  <cameraHelper args={camera}/>
+  <orbitControls ref={controls} args={[camera, domElement]} autoRotate={false} enableZoom={true} />
+  </group>
+  )
 
 })
 
 function App() {
     const mouse = useRef([0, 0])
-
+    // add buttons to switch 
     return (
         <Canvas style={{height: 500, color: "FF0000"}}>
             <CameraControls/>            
-            <LoaderTest/>
             <scene name="Scene">
             <ambientLight />
             <pointLight position={[0, 400, 0]} intensity={4} distance={1000} decay={2} color={"white"}/>
             <pointLight position={[0, -400, 0]} intensity={4} distance={1000} decay={2} color={"white"}/>
             <pointLight position={[0, 0, 400]} intensity={4} distance={1000} decay={2} color={"white"}/>
             <pointLight position={[0, 0, -400]} intensity={4} distance={1000} decay={2} color={"white"}/>
-            <Particles/>
+            {/* <Particles/>        */}
+            <Suspense fallback={null}>     
+            <LoaderTest/>
+            </Suspense>
             {/* <BoxTest/> */}
             {/* <BoxTwo/> */}
           {/* <Swarm mouse={mouse} count={20000} />
