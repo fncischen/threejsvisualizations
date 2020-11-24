1e151d118027dcbe224be10c8050d18a3795bec8
fncischen <fncis.chen@gmail.com>
2020-11-23 18:29:44 -0800

    removed extra camera

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ extend({ OrbitControls });
 
 // https://github.com/dataarts/dat.gui/blob/master/API.md
 
-var testmeout;
-
 const CameraControls = ((props) => {
 
     // Get a reference to the Three.js Camera, and the canvas html element.
@@ -25,9 +23,6 @@ const CameraControls = ((props) => {
     gl,
   } = useThree()
 
-  console.log(useThree());
-  console.log(gl);
-  console.log(gl.domElement);
   // console.log(scene);
 
   const params = {
@@ -37,11 +32,6 @@ const CameraControls = ((props) => {
 
   const controls = useRef();  
   
-  let SCREEN_WIDTH = window.innerWidth;
-  let SCREEN_HEIGHT = window.innerHeight;
-  let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-
-  gl.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
   // const Camera = new THREE.PerspectiveCamera({fov: 60, position: [0, 600, 600], near: 0.1, far: 5000});
   camera.position.x = 0;
   camera.position.y = 15;
@@ -49,24 +39,6 @@ const CameraControls = ((props) => {
   camera.fov = 60;
   camera.near = 0.1;
   camera.far = 10000;
-  camera.aspect = aspect * 0.5;
-
-
-
-
-  let camera2 = new THREE.PerspectiveCamera(60, aspect * 0.5, 0.1, 1000);
-  camera2.position.x = 0;
-  camera2.position.y = 0;
-  camera2.position.z = 5;
-  console.log(camera)
-  console.log(camera2);
-    // renderer.clear();
-
-    // renderer.setViewport(0, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT);
-    // renderer.render(scene, camera);
-
-    // renderer.setViewport(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT );
-    // renderer.render(scene, camera2);
 
   var gui = new dat.GUI();
   gui.add(params, "splitScreen") // add boolean to toggle orthogonal camera or regular camera 
@@ -80,36 +52,22 @@ const CameraControls = ((props) => {
 
   // figure out the react render viewport issues
   // https://shaderism.com/multiple-render-views-with-react
-  const animate = (() => {
-    gl.clear();
 
-    if(params['switchToPublicView'] == true) {
-          var cam = camera; 
-          gl.render(scene, cam); 
-    }
-    else {
-         var cam = camera2;
-         gl.render(scene, cam)
-    }
-    
-  })
   
-  testmeout = gl; 
+  // testmeout = gl; 
 
   useFrame(() => {    
-    requestAnimationFrame(animate);
+    // requestAnimationFrame(animate);
 
     controls.current.update()
-    animate(); // set up loop function for animation 
+    // animate(); // set up loop function for animation 
 
 
   })
-
 
   return( 
   <group>
   <cameraHelper args={camera}/>
-  <cameraHelper args={camera2}/>
   <orbitControls ref={controls} args={[camera, gl.domElement]} autoRotate={false} enableZoom={true} />
   </group>
   )
@@ -119,30 +77,8 @@ const CameraControls = ((props) => {
 function App() {
     const mouse = useRef([0, 0])
 
-    // const {
-    //   camera,
-    // } = useThree()
-
-    // console.log(useThree());
-
-    // let SCREEN_WIDTH = window.innerWidth;
-    // let SCREEN_HEIGHT = window.innerHeight;
-    // let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-
-    // var scene = new THREE.Scene();
-
-    // let camera2 = new THREE.PerspectiveCamera({fov: 60, near: 0.1, far: 50000, position: new THREE.Vector3(30,30,0) });
-
-    // var renderer = new THREE.WebGLRenderer();
-    // renderer.setViewport(0, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT);
-    // renderer.render(scene, camera);
-
-    // renderer.setViewport(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT );
-    // renderer.render(scene, camera2);
-    // console.log(renderer);
-    // add buttons to switch 
     return (
-        <Canvas gl={testmeout} style={{height: 500, color: "FF0000"}}>
+        <Canvas style={{height: 500, color: "FF0000"}}>
             <CameraControls/>            
             <ambientLight />
             <pointLight position={[0, 400, 0]} intensity={4} distance={1000} decay={2} color={"white"}/>
