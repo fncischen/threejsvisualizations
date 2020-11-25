811458fca2123a7749391c2a775e29b87ad00fdc
fncischen <fncis.chen@gmail.com>
2020-11-24 21:34:25 -0800

    setting up react hooks for scroll events

--- src/components/storyTypes/verticalStory/verticalStoryDashboard.js
+++ src/components/storyTypes/verticalStory/verticalStoryDashboard.js
@@ -1,22 +1,23 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
+import React, { Component } from 'react'
 
 // insert a line to import data from JSON file
 
-export default class VerticalStoryDashboard {
+export default class VerticalStoryDashboard extends Component {
 
     constructor(props) {
-        this.storyPaths = props.storyPaths; 
-        this.stories = [];
-        this.cameraPosition = props.cameraPosition;
-        this.camera = null; 
+        super(props);
+        // this.storyPaths = props.storyPaths; 
+        // this.stories = [];
+        // this.cameraPosition = props.cameraPosition;
+        // this.camera = null; 
         
-        this.dyVelocity = 3;
-        this.dxVelocity = 5; 
+        // this.dyVelocity = 3;
+        // this.dxVelocity = 5; 
 
-        this.loadStories();
-        this.initalizeStory();
+        // this.loadStories();
+        // this.initalizeStory();
         this.setupEventlisteners();
     }
 
@@ -42,70 +43,31 @@ export default class VerticalStoryDashboard {
         // }
     }
 
-    onCameraUpdate(){
-        this.camera.position.x = window.scrollX/this.dxVelocity;
+    onCameraUpdate(event){
+        console.log(event);
+        // this.camera.position.x = window.scrollX/this.dxVelocity;
     }
 
     initializeCamera() {
         this.camera = new THREE.Camera({fov: 60, position: this.cameraPosition, near: 0.1, far: 5000})
     }
 
-    // convert 3d objects from storySpace to worldSpace
-    convertStorySpaceToWorldSpace(storyIndex) {
-        let story = this.stories[index];
-
-        // assume ground plane is imported from box 
-        let groundPlane = story.groundPlane; 
-        let groundPlaneWidth = groundPlane.width; 
-        
-        let groundPlaneCoordsX = groundPlaneWidth +  this.retrieveStoryPosX(index); 
-        let groundPlaneCoordsY = this.retrieveStoryPosY();
-        let groundPlaneCoordsZ = 0;
-
-        let groundPlanePos = new THREE.Vector3(groundPlaneCoordsX,groundPlaneCoordsY, groundPlaneCoordsZ);  
-        // refactor 
-        for(var i = 0; i < story.characters.length; i++){
-            let character = story.characters[i];
-
-            // access character 3d object;
-            character.position += groundPlanePos;
-        }
-
-        for(var j = 0; j < story.environments.length; j++) {
-            let environment = story.environment[j];
-            
-            // access character 3d object;
-            environment.position += groundPlanePos; 
-            // convert from story space to world space;
-
-        }
-    }
-
-    retieveStoryPosY(){
-        let y = this.story[0].height/2; 
-        return y; 
-    }   
-
-    retrieveStoryPosX(index){
-        let width = 0; 
-
-        for(var i = 0; i < this.stories.length; i++){
-            width += this.stories[i].width;
-        }
-        return width; 
-    }
-
-    initalizeStory() {
-        this.initializeCamera();
-        for(var i = 0; i < this.stories.length; i++){
-            this.convertStorySpaceToWorldSpace(i);
-        }
-    }
-
     setupEventlisteners() {
         
-        window.addEventListener('scroll', this.onCameraUpdate);
-
+        // let root = document.querySelector('#root');
+        // console.log(root );
+        // root.onscroll = this.onCameraUpdate;
+        // console.log(window);
+        // console.log(root.onscroll);
+        window.addEventListener('scroll', (e) => function(e){
+            console.log('is this working');
+            console.log(e);
+        })
     }
 
+    render() {
+        return (
+            <div className="test2"></div>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import ReactDOM from 'react-dom'
-import React, {Suspense,useRef} from 'react'
+import React, {Suspense, Component, useRef} from 'react'
 import Particles from "./components/particles/particles.js"
 import { Canvas, useFrame, useThree, extend} from 'react-three-fiber'
 import LoaderTest from "./components/storyTypes/verticalStory/loaderTest.js"
 import * as THREE from 'three'
 
 import CameraControls from "./components/cameraControls/cameraControls.js"
+import VerticalStoryDashboard from "./components/storyTypes/verticalStory/verticalStoryDashboard.js"
 
-function App() {
+class App extends React.Component {
 
-    return (
+    constructor(props){
+      super(props);
+
+      this.state = { previousY: 0.0, deltaY: 0.0 }
+      console.log(this.state);
+
+    }
+
+    // this is a better way of rendering events 
+    componentDidMount() {
+      window.addEventListener('scroll', this.handleScroll);
+
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    // check for hooks 
+    // https://stackoverflow.com/questions/57088861/react-setstate-hook-from-scroll-event-listener
+    handleScroll(e){
+      console.log("curent state :" + this.state);
+
+      var newScrollY = window.scrollY;
+      var dY = newScrollY - this.state.previousY;
+      await this.setState({previousY: newScrollY, deltaY: dY})
+      setInterval(1000);
+      // take the delta between previous scrollY and current scrollY
+      // send it to the canvas object's camera
+
+      // use it to pan the camera 
+
+      // retrive scroll data, send it to state, and then place it inside the vertical story dashboard 
+    }
+
+    render() {
+    return (            
+        <div className="test">
+        <VerticalStoryDashboard/>
         <Canvas style={{height: 500, color: "FF0000"}}>
-            <CameraControls/>            
+            {/* <CameraControls/>             */}
             <ambientLight />
             <pointLight position={[0, 400, 0]} intensity={4} distance={1000} decay={2} color={"white"}/>
             <pointLight position={[0, -400, 0]} intensity={4} distance={1000} decay={2} color={"white"}/>
@@ -27,7 +66,9 @@ function App() {
           <Effect />
           <Dolly /> */}
         </Canvas>
+        </div>
     )
+    }
   }
   
   ReactDOM.render(<App />, document.getElementById('root'))
